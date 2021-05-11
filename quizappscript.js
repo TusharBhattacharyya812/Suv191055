db13f8867417aea73be6e6e0dfe1ae426fd871f5
TusharBhattacharyya812 <57327468+TusharBhattacharyya812@users.noreply.github.com>
2021-05-11 06:40:55 +0530

    Update quizappscript.js

--- quizappscript.js
+++ quizappscript.js
@@ -47,9 +47,58 @@ const option2 = document.querySelector('#option2');
 const option3 = document.querySelector('#option3');
 const option4 = document.querySelector('#option4');
 const submit = document.querySelector('#submit');
+const answers = document.querySelectorAll('.answer');
+const showScore = document.querySelector('#showScore')
 
+let questionCount = 0;
+let score = 0;
 const loadQuestion = () => {
-    question.innerText = quizDB[0].question;
+    const questionList = quizDB[questionCount];
+    
+    question.innerText = questionList.question;
+    
+    option1.innerText = questionList.a;
+    option2.innerText = questionList.b;
+    option3.innerText = questionList.c;
+    option4.innerText = questionList.d;
 
 }
+
 loadQuestion();  
+
+const getCheckAnswer = () => {
+    let answer;
+
+    answers.forEach((curAnsElem) => {
+        if (curAnsElem.checked){
+            answer = curAnsElem.id;
+        }
+        });  
+
+        return answer;
+    };
+
+const deselectAll = () => {
+    answers.forEach((curAnsElem) => curAnsElem.checked = false);
+}
+submit.addEventListener('click', () => {
+    const checkedAnswer = getCheckAnswer();
+    console.log(checkedAnswer);
+
+if (checkedAnswer == quizDB[questionCount].ans){
+    score++;
+};
+questionCount++
+
+deselectAll();
+if (questionCount < quizDB.length){
+    loadQuestion();
+}
+else {
+    showScore.innerHTML = `
+    <h3>You scored ${score}/${quizDB.length}✌️</h3> 
+       <button class="btn" onclick="location.reload()">Play Again</button>
+       `;
+}
+
+});
